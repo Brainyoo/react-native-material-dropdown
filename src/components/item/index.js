39a63a1c3a87865263d8fa33cd41a863667d4ce8
André <andre.boerger@lyth.de>
2020-02-06 14:41:40 +0100

    testid for dropdownitems

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,21 +1,22 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Button } from 'react-native-material-buttons';
+import React, { PureComponent } from "react";
 
-import styles from './styles';
+import { Button } from "react-native-material-buttons";
+import PropTypes from "prop-types";
+import styles from "./styles";
 
 export default class DropdownItem extends PureComponent {
   static defaultProps = {
-    color: 'transparent',
-    disabledColor: 'transparent',
+    color: "transparent",
+    disabledColor: "transparent",
     rippleContainerBorderRadius: 0,
-    shadeBorderRadius: 0,
+    shadeBorderRadius: 0
   };
 
   static propTypes = {
     ...Button.propTypes,
 
     index: PropTypes.number.isRequired,
+    testID: PropTypes.string.isRequired
   };
 
   constructor(props) {
@@ -27,18 +28,18 @@ export default class DropdownItem extends PureComponent {
   onPress() {
     let { onPress, index } = this.props;
 
-    if ('function' === typeof onPress) {
+    if ("function" === typeof onPress) {
       onPress(index);
     }
   }
 
   render() {
-    let { children, style, index, ...props } = this.props;
+    let { children, style, index, testID, ...props } = this.props;
 
     return (
       <Button
         {...props}
-
+        testID={testID + "-" + index}
         style={[styles.container, style]}
         onPress={this.onPress}
       >
